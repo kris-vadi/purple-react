42dd6e46b91661508c0256559bf32cfce4d35cf0
Kristina Morozova <christina.vadi@gmail.com>
2024-10-17 12:45:48 +0500

    feat: add Button component

--- kinopoisk/src/App.jsx
+++ kinopoisk/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 
 import Title from './components/Title/Title';
+import Button from './components/Button/Button';
 
 function App() {
 	const text = 'Поиск';
+	const buttonText = 'Искать';
 
 	return (
 		<>
 			<Title text={text} />
+			<Button buttonText={buttonText}/>
 		</>
 	);
 }
